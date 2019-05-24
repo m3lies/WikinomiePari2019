b5f884f132e6e90ae7b71283906aaf4698044206
laurana <laurana.dc@gmail.com>
2019-05-24 15:07:40 +0200

    Déclaration variable

---
+++ MetaCoinPari/contracts/Paris.sol
@@ -0,0 +1,14 @@
+pragma solidity >=0.4.25 <0.6.0;
+
+contract Paris{
+
+
+    uint256 public minimumBet;
+    uint256 public totalBetOne;
+    uint256 public totalBetTwo;
+    uint256 public
+
+
+
+
+}
