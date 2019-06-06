33cf9ad27c95d530be62b2be1895e185941da4fe
Melanie Manivanh <melanie.manivanh@gmail.com>
2019-06-06 10:58:08 +0200

    recommencé à 0

--- MetaCoinPari/contracts/Paris.sol
+++ MetaCoinPari/contracts/Paris.sol
@@ -3,23 +3,4 @@ pragma solidity >=0.4.25 <0.6.0;
 contract Paris{
 
 
-    uint256 minimumBet = 0;
-    uint256 public totalBetOne;
-    uint256 public totalBetTwo;
-    uint256 public numberOfBets;
-    uint256 public maxAmountOfBets = 1000;
-    address payable[] public players;
-
-    struct Player{
-        uint256 amountBet;
-        uint16 teamSelected;
-    }
-
-    //Adress of the player and => the user info
-    mapping(address => Player) public playerInfo;
-
-
-
-
-
 }
